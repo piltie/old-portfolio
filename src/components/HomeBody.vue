344e51d5eda47de373baf9e336e8e809ac3543e2
Sarah Carla Souza <95034183+piltie@users.noreply.github.com>
2022-09-29 16:57:36 -0300

    Adding tape images and home text

--- src/components/HomeBody.vue
+++ src/components/HomeBody.vue
@@ -1,9 +1,19 @@
 <template>
-  <div class="py-[4em]">
-    <img
-      class="w-[15em] m-auto border-solid border-[0.5em] border-[white] border-b-[3em]"
-      src="../assets/images/sarah.jpg"
-    />
+  <div class="py-[4em] font-NotoSansMono font-normal">
+    <div
+      class="w-[15em] m-auto border-solid border-[0.5em] border-[white] border-b-[1em] bg-[white]"
+    >
+      <img
+        class="absolute w-[8em] ml-[-3em] mt-[-2em]"
+        src="../assets/images/tape.png"
+      />
+      <img
+        class="absolute w-[8em] ml-[10em] mt-[17em]"
+        src="../assets/images/tape2.png"
+      />
+      <img src="../assets/images/sarah.jpg" />
+      <h1 class="text-center py-[1em] text-black-pastel">Sarah Carla Souza</h1>
+    </div>
   </div>
 </template>
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,7 @@ module.exports = {
       fontFamily: {
         Poppins: "Poppins",
         JosefinSlab: "Josefin Slab",
+        NotoSansMono: "Noto Sans Mono",
       },
       colors: {
         yellow: {
